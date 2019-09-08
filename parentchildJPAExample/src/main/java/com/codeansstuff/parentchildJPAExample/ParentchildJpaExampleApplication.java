343852cee657300fc248ed5d0b5f0760d0a44c24
Malkeith <kthsingh.ms@gmail.com>
2019-09-08 22:29:17 +0530

    Initial commit

---
+++ parentchildJPAExample/src/main/java/com/codeansstuff/parentchildJPAExample/ParentchildJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.codeansstuff.parentchildJPAExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentchildJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentchildJpaExampleApplication.class, args);
+	}
+
+}
---
+++ parentchildJPAExample/src/test/java/com/codeansstuff/parentchildJPAExample/ParentchildJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeansstuff.parentchildJPAExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentchildJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
